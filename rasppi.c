eab962d21fcd1a942e6de4f1fd33438e82360ec1
berryberryproject <35187256+berryberryproject@users.noreply.github.com>
2018-01-08 18:27:14 +0900

    Update rasppi.c

--- rasppi.c
+++ rasppi.c
@@ -154,7 +154,7 @@ int main(int argc, char* argv[])
 	if(Key_IN=linux_kbhit())
 	{	
 		fputc(KEY_DOWN, DEBUG);
-		fputs("now the result",DEBUG);
+		fputs=c(",",DEBUG);
 		fputc(Key_IN, DEBUG);
 		switch (Key_IN)
 		{
