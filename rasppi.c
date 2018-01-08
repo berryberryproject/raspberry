2875cfd99ab123e750d9d78881ce46bf03b81915
berryberryproject <35187256+berryberryproject@users.noreply.github.com>
2018-01-08 17:05:51 +0900

    Update rasppi.c

--- rasppi.c
+++ rasppi.c
@@ -395,7 +395,8 @@ int n_choices, i;
 ITEM *Current_Slected;
 //아이템 생성----------
 
-n_choices = ARRAY_SIZE((choices));
+//n_choices = ARRAY_SIZE((choices));
+n_choices = 7;
 TABLE = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
 	for(i = 0; i < n_choices; ++i)
 	        TABLE[i] = new_item(choices[i], choices[i]);
@@ -405,7 +406,6 @@ TABLE = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
 
 set_menu_win(Menu,SLECT_W);
 //set_menu_sub(Menu, derwin(SLECT_W, n_choices,SLECT_WIDTH,SLECT_HEIGHT - n_choices,0)); //(상자크기(y,x 위치 y,x)
-set_menu_sub(Menu, derwin(SLECT_W, n_choices,SLECT_WIDTH,SLECT_HEIGHT- 7,0)); //(상자크기(y,x 위치 y,x)
 set_menu_mark(Menu, ">");
 wattron(SLECT_W,COLOR_PAIR(PAIR_RED_BLUE));
 //box(SLECT_W, 0, 0);
