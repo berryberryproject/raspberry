e5ff43a462960db45c2e321ee9dfd17272ded694
berryberryproject <35187256+berryberryproject@users.noreply.github.com>
2018-01-18 22:54:57 +0900

    Update rasppi.c

--- rasppi.c
+++ rasppi.c
@@ -634,27 +634,27 @@ char*locate_shared_data(SHARED_DATA *shared_data,int i)
 {
 if(i ==1)
 {
-return (shared_data.DATA1);	
+return (char*)((*shared_data).DATA1);	
 }
 else if(i==2)
 {
-return (shared_data.DATA2);	
+return (char*)((*shared_data).DATA2);	
 }
 else if(i==3)
 {
-return (shared_data.DATA3);	
+return (char*)((*shared_data).DATA3);	
 }
 else if(i==4)
 {
-return (shared_data.DATA4);	
+return (char*)((*shared_data).DATA4);	
 }
 else if(i==5)
 {
-return (shared_data.DATA5);	
+return (char*)((*shared_data).DATA5);	
 }
 else if(i==6)
 {
-return (shared_data.DATA6);	
+return (char*)((*shared_data).DATA6);	
 }
 	
 	
