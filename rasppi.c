bc8907d5a9573c8e670eeea724c1b9dc6e1fc944
berryberryproject <35187256+berryberryproject@users.noreply.github.com>
2018-01-19 23:29:07 +0900

    Update rasppi.c

--- rasppi.c
+++ rasppi.c
@@ -253,7 +253,7 @@ exit(1);
 pid=fork();		
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 if(pid==0)
-{
+{	dup2(pipe_fd[1],2);
 	dup2(pipe_fd[1],1);
 	if(execl("/usr/sbin/tcpdump","/usr/sbin/tcpdump","-U",NULL) ==-1)
 	{
@@ -754,7 +754,8 @@ MENU* create_newslectwin(WINDOW* SLECT_W, char** choices, int SLECT_WIDTH, int S
 	MENU *Menu_o;
 	int n_choices, i;
 	ITEM *Current_Slected;
-	n_choices = sizeof(*choices) / sizeof(*choices[0]);
+	n_choices = 6;
+	//n_choices = sizeof(*choices) / sizeof(*choices[0]);
 	TABLE = (ITEM **)calloc(n_choices + 1, sizeof(ITEM *));
 	for (i = 0; i < n_choices; ++i)
 		TABLE[i] = new_item(choices[i], choices[i]);
