874cf5cd894239fb4237b0bb754638bc2100a22e
berryberryproject <35187256+berryberryproject@users.noreply.github.com>
2018-04-07 22:11:04 +0900

    Update workassign.sh

--- workassign.sh
+++ workassign.sh
@@ -139,7 +139,10 @@ if [ -f "$SIMUL" ] ; then
 cat "download/$NAME/$(ls download/$NAME/ -F|grep '[*]'|awk -v var=$CNT_L 'FNR==var'|cut -d. -f2|cut -d* -f1).c" >> download/$NAME/SIMUL.txt 2>&1
 echo "-----------------------------------------------------------------------------" >> download/$NAME/SIMUL.txt 2>&1
 echo "-----------------------------------------------------------------------------" >> download/$NAME/SIMUL.txt 2>&1
-
+echo "현재입력은 다음과 같습니다"
+head -n 14 $SIMUL >> download/$NAME/SIMUL.txt 2>&1
+echo "-----------------------------------------------------------------------------" >> download/$NAME/SIMUL.txt 2>&1
+echo "-----------------------------------------------------------------------------" >> download/$NAME/SIMUL.txt 2>&1
 timeout 0.1 ./download/$NAME/"$(ls download/$NAME/ -F|grep '[*]'|awk -v var=$CNT_L 'FNR==var'|cut -d* -f1)" < $SIMUL >> download/$NAME/SIMUL.txt 2>&1
 echo "-----------------------------------------------------------------------------" >> download/$NAME/SIMUL.txt 2>&1
 echo "-----------------------------------------------------------------------------" >> download/$NAME/SIMUL.txt 2>&1
